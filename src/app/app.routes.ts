3aa696257b0c23f042c5165dfc81bd22bebe59b9
kubilay-fh <kubilaysabah@gmail.com>
2024-09-12 18:34:37 +0300

    create angular 18 app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./list/list.component').then(c => c.ListComponent)
+  },
+  {
+    path: ':id',
+    loadComponent: () => import('./detail/detail.component').then(c => c.DetailComponent),
+  }
+];
